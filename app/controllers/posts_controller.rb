cdd536f9d289f4f78dbf06e747176ba241a6dc20
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-11 16:04:53 +0500

    edit new and create action for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,20 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def new
     @post = Post.new
   end
 
-  def create
 
+  def create
+    @post = Post.new(title: params[:title], body: params[:body])
+    if @post.save
+      redirect_to new_post_path
+    else
+      render :new
+    end
   end
 
   def index
-    
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,12 @@
-ActiveRecord::Schema.define(version: 2020_11_10_132019) do
+ActiveRecord::Schema.define(version: 2020_11_11_103929) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -15,6 +17,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_132019) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
