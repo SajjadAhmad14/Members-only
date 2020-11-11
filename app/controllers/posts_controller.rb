ee67c125173e03441e88af12bd0673608ea08887
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-11 21:02:31 +0500

    add name of author

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,24 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
-
   def create
-    @post = Post.new(title: params[:title], body: params[:body])
+    @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to new_post_path
+      redirect_to posts_path
     else
       render :new
     end
   end
 
   def index
+    @posts = Post.all
   end
 
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
