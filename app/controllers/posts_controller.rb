4349288fa37040b7f564158f8c47b4813575f733
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-12 15:19:40 +0500

    change style of nav bar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def new
-    @post = current_user.posts.build
+    @post = current_user.posts.new
   end
 
   def create
-    @post = current_user.posts.build(post_params)
+    @post = current_user.posts.new(post_params)
     if @post.save
       redirect_to posts_path
     else
